ffbf3ea2e11342edba68bfe255b96bbb9ef4e90e
Matt Cockayne <matt@zucchi.co.uk>
2012-07-17 19:07:03 +0100

    updated classmap

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -2,6 +2,7 @@
 return array(
     'Zucchi\Form\View\Helper\BootstrapForm'              => __DIR__ . '/src/Zucchi/Form/View/Helper/BootstrapForm.php',
     'Zucchi\Form\View\Helper\BootstrapRow'              => __DIR__ . '/src/Zucchi/Form/View/Helper/BootstrapRow.php',
+    'Zucchi\Form\View\Helper\BootstrapCollection'              => __DIR__ . '/src/Zucchi/Form/View/Helper/BootstrapCollection.php',
      
     'Zucchi\View\Helper\Truncate'              => __DIR__ . '/src/Zucchi/View/Helper/Truncate.php',
 );
