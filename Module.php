209cf2dcd8078dc83484992174dc4c5314ec4e20
Victor Guedes <victorguedes@gmail.com>
2014-01-31 09:34:35 -0200

    Update Module.php

--- Module.php
+++ Module.php
@@ -44,7 +44,7 @@ class Module implements
         return include __DIR__ . '/config/module.config.php';
     }
 
-    public function getServiceConfig ()
+    public function getViewHelperConfig()
     {
         return array(
             'factories' => array(
