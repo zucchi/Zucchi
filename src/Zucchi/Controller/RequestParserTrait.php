38c18ed5060b14d5554d8ed7a3931e048af46672
Matt Cockayne <matt@zucchi.co.uk>
2015-10-30 15:13:10 +0000

    adds better support for string based where statements & complex expressions

--- src/Zucchi/Controller/RequestParserTrait.php
+++ src/Zucchi/Controller/RequestParserTrait.php
@@ -17,9 +17,9 @@ trait RequestParserTrait
      * a format usable by the getList() method of the services.
      * @return array
      */
-    protected function parseWhere(array $where = array())
+    protected function parseWhere($where = false)
     {
-        $clauses = array(
+        $operators = array(
             'is'    => 'is',
             'eq'    => '=',
             'gt'    => '>',
@@ -32,20 +32,83 @@ trait RequestParserTrait
             'regex' => 'regexp',
         );
 
+        $modes = array(
+            'or' => 'or',
+            'and' => 'and',
+        );
+
+        if (is_array($where)) {
+            if (array_key_exists('expressions', $where)) {
+                $where = $this->parseComplexWhere($where, $operators, $modes);
+            } else {
+                $where = $this->parseSimpleWhere($where, $operators, $modes);
+            }
+        } else if (!is_string($where)) {
+            $where = false;
+        }
+
+        return $where;
+    }
+
+
+    /**
+     * @param $where
+     * @param $operators
+     * @param $modes
+     * @return array
+     */
+    protected function parseComplexWhere($where, $operators, $modes)
+    {
+        if (array_key_exists('expressions', $where)) {
+
+            if (isset($where['mode']) && isset($modes[$where['mode']])) {
+                $where['mode'] = $modes[$where['mode']];
+            } else {
+                $where['mode'] = $modes['and'];
+            }
+
+            foreach ($where['expressions'] as $index => &$expression) {
+                if (isset($expression['mode']) && isset($modes[$expression['mode']])) {
+                    $expression['mode'] = $modes[$expression['mode']];
+                } else {
+                    $expression['mode'] = $expression['and'];
+                }
+                $expression['fields'] = $this->parseSimpleWhere($expression['fields'], $operators, $modes);
+            }
+        }
+
+        return $where;
+    }
+
+    /**
+     * @param $where
+     * @param $operators
+     * @param $modes
+     * @return array
+     */
+    protected function parseSimpleWhere($where, $operators, $modes)
+    {
         // loop through and sanitize the where statement
         foreach ($where as $field => &$value) {
             if (is_array($value)) {
                 if (isset($value['value']) && is_string($value['value']) && strlen($value['value'])) {
-                    if (isset($value['operator']) && isset($clauses[$value['operator']])) {
-                        $value['operator'] = $clauses[$value['operator']];
+                    if (isset($value['operator']) && isset($operators[$value['operator']])) {
+                        $value['operator'] = $operators[$value['operator']];
                     } else {
-                        $value['operator'] = '=';
+                        $value['operator'] = $operators['eq'];
+                    }
+
+                    if (isset($value['mode']) && isset($modes[$value['mode']])) {
+                        $value['mode'] = $modes[$value['mode']];
+                    } else {
+                        $value['mode'] = $modes['and'];
                     }
                 }
-            } else if (is_string($value) && strlen($value)){
+            } else if (is_string($value) && strlen($value)) {
                 $value = array(
-                    'operator'  => '=',
-                    'value'     => $value
+                    'mode' => $modes['and'],
+                    'operator' => $operators['eq'],
+                    'value' => $value
                 );
             }
         }
